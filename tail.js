137c654e8dba6f533b92a48eeca46ebc81561364
Kelsi Olstad <kelsiolstad@gmail.com>
2022-02-07 19:30:37 -0700

    first commit

---
+++ tail.js
@@ -0,0 +1,17 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`⛔️⛔️⛔️Assertion Failed:${actual} !== "${expected}`);
+  }
+};
+
+const tail = function(arr) {
+  let tailArray = arr.slice(1);
+  return tailArray;
+};
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs");
