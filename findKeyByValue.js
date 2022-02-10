302c5c8d55aaf5f7ea106bfc180e08176d1c29a8
Kelsi Olstad <kelsiolstad@gmail.com>
2022-02-09 20:23:49 -0700

    check values and return key

---
+++ findKeyByValue.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`⛔️⛔️⛔️Assertion Failed:${actual} !== "${expected}`);
+  }
+};
+
+//should scan the object and return the first key which contains the given value. If no key with that given value is found, then it should return undefined.
+
+const findKeyByValue = function(obj, value) {
+  for (const key of Object.keys(obj)) {
+    const objValue = obj[key];
+    if (objValue === value) {
+      return key;
+    }
+  }
+};
+
+const myMonster = {
+  name: "Medusa",
+  hair: "snakes",
+  power: "turn to stone"
+};
+
+// console.log(myMonster["hair"]); //keys are string!!
+console.log(findKeyByValue(myMonster, "Medusa"));
+assertEqual(findKeyByValue(myMonster, "turn to stone"), "power");
+assertEqual(findKeyByValue(myMonster, "fire"), undefined);
