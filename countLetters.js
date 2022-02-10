42fd460a70bbd15b28aaeecd871b1e071e9583d2
Kelsi Olstad <kelsiolstad@gmail.com>
2022-02-09 18:04:41 -0700

    count letters function

---
+++ countLetters.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`⛔️⛔️⛔️Assertion Failed:${actual} !== "${expected}`);
+  }
+};
+
+const countLetters = function(str) {
+  let results = {}
+  for (const letter of str) {
+    if (letter !== " ") {
+      if(results[letter]) {
+        results[letter] += 1;
+      } else {
+        results[letter] = 1;
+      }
+    }
+  }
+  return results;
+}
+
+console.log(countLetters('Lighthouse in the house'));
+
+
