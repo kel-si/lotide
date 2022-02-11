f601883c7b7523a6a2e8053c24646276fc8f600d
Kelsi Olstad <kelsiolstad@gmail.com>
2022-02-10 19:43:58 -0700

    created map function

---
+++ map.js
@@ -0,0 +1,17 @@
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+    // console.log('item BEFORE: ', item);
+    // console.log('item AFTER: ', callback(item));
+    console.log('---');
+  }
+  return results;
+};
+
+const mapFunc = word => word[0];
+
+const results1 = map(words, mapFunc);
+console.log(results1);
