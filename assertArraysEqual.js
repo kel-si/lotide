580014d44daa4f9c91e4b340cc637b7176f401a6
Kelsi Olstad <kelsiolstad@gmail.com>
2022-02-14 13:59:34 -0700

    refactor to include modules

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,19 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`⛔️⛔️⛔️Assertion Failed:${actual} !== "${expected}`);
-  }
-};
-
-const eqArrays = function(arr1, arr2) {
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i] || arr1.length !== arr2.length) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
+const assertEqual = require('./assertEqual');
 
 const assertArraysEqual = function(actual, expected) {
   const isEqual = eqArrays(actual, expected);
@@ -24,4 +10,5 @@ const assertArraysEqual = function(actual, expected) {
     console.log('The arrays do not match.');
   }
 };
-assertArraysEqual([1,2,3], [1,2,3]);
+
+module.exports = assertArraysEqual;
