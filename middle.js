52fdb44afab3efc900fd0761f76b88103751e081
Kelsi Olstad <kelsiolstad@gmail.com>
2022-02-14 14:24:23 -0700

    refactor using modules

--- middle.js
+++ middle.js
@@ -1,20 +1,5 @@
-const eqArrays = function(arr1, arr2) {
-  for (let i = 0; i < arr1.length; i++) {
-    if(arr1[i] !== arr2[i] || arr1.length !== arr2.length) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(actual, expected) {
-  const isEqual = eqArrays(actual, expected);
-  if (isEqual === true) {
-    console.log('The arrays match!');
-  } else {
-    console.log('The arrays do not match.')
-  }
-};
+const eqArrays = require("./eqArrays");
+const assertArraysEqual = require("./assertArraysEqual");
 
 const middle = function(arr) {
   let midPoint = (arr.length/2);
@@ -36,14 +21,4 @@ const middle = function(arr) {
     }
 };
 
-console.log(middle([1,2,3,4,5,6,7,8]));
-console.log(middle([1,2,3,4,5]));
-console.log(middle([]));
-console.log(middle([1,2]));
-
-
-
-
-
-// arrays with one or two elements, there is no middle. Return an empty array.
-
+module.exports = middle;
