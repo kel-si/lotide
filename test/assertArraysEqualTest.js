aea382b12a11bea07db646702f1311feaabe8f6d
Kelsi Olstad <kelsiolstad@gmail.com>
2022-02-14 14:00:20 -0700

    tests after refactoring with modules

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,6 @@
+const eqArrays = require('../eqArrays');
+const assertArraysEqual = require('../assertArraysEqual');
+const assertEqual = require('../assertEqual');
+
+
+assertArraysEqual([1,2,3], [1,2,3]);
