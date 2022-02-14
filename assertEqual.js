31aed87c4472c04727235d13f89209b9ad7fbc34
Kelsi Olstad <kelsiolstad@gmail.com>
2022-02-14 10:43:52 -0700

    refactor code using modules to share code

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,13 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+    console.log(`💚💚💚Assertion Passed: ${actual} === ${expected}`);
   } else {
     console.log(`⛔️⛔️⛔️Assertion Failed:${actual} !== "${expected}`);
   }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+module.exports = assertEqual;
+
+
+
+
