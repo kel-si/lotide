4675c6f172abfdec4f29f42ad4331597343e1b26
Kelsi Olstad <kelsiolstad@gmail.com>
2022-02-14 16:16:06 -0700

    delete unnecessary code

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,12 +1,5 @@
 const middle = require("../middle");
-// const eqArrays = require("../eqArrays");
-// const assertArraysEqual = require("../assertArraysEqual");
 const assert = require('chai').assert;
-
-// assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8]), [4, 5]);
-// assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-// assertArraysEqual(middle([]), []);
-// assertArraysEqual(middle([1, 2]), []);
 
 describe("#middle", () => {
   it("returns an empty array when given an array with only two elements", () => {
